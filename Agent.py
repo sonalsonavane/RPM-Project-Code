0ec7ef152de49612a434cf9924a1abef571af171
Sonavane, Sonal R <ssonavane3@gatech.edu>
2023-06-18 00:04:08 -0700

    Good Progress for Similar images.

--- Agent.py
+++ Agent.py
@@ -9,8 +9,10 @@
 # These methods will be necessary for the project's main method to run.
 
 # Install Pillow and uncomment this line to access image processing.
-#from PIL import Image
-#import numpy
+from PIL import Image
+
+import numpy
+
 
 class Agent:
     # The default constructor for your Agent. Make sure to execute any
@@ -30,5 +32,34 @@ class Agent:
     #
     # Make sure to return your answer *as an integer* at the end of Solve().
     # Returning your answer as a string may cause your program to crash.
-    def Solve(self,problem):
-        return -1
+    def Solve(self, problem):
+        if problem.problemType == "2x2" and "Problems B" in problem.problemSetName:
+            ans = self.solve_2x2(problem)
+            if ans != -1:
+                return ans
+        else:
+            return 1
+
+    def solve_2x2(self, ravens_problem):
+        problem = []
+        choices = []
+
+        for key, value in ravens_problem.figures.items():
+            if key in ['A', 'B', 'C']:
+                problem.append(value)
+            elif key in ['1', '2', '3', '4', '5', '6']:
+                choices.append(value)
+        problem = self.load_images(problem)
+        choices = self.load_images(choices)
+
+        return 0
+
+    def get_pixel_difference(self, image1, image2):
+        return numpy.sum(numpy.abs(image1 - image2))
+
+    def load_images(self,images):
+        images = []
+        for image in images:
+            img = Image.open(image.visualFilename).convert("L")  # Convert to grayscale
+            images.append(numpy.array(img))
+        return images
